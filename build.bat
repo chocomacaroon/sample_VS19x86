5a9222f6911bab1196a196be4c22445b51111c0b
Taehui Kwon <kwonk17@naver.com>
2025-12-18 15:47:35 +0900

    Fixed bug for demo

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.com" C:\Users\MDSIT\Desktop\sample_VS19x86\sample_VS19x86.sln /rebuild
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.com" .\sample_VS19x86.sln /rebuild
